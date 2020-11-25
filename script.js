d432893fd2cc4ca30ed6614c676ff26780c7a43f
tien96ng <tien96ng@gmail.com>
2020-11-25 12:55:46 -0800

    Can render book elements but not display.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Variables to call Elements by ID.
 const showFormBtn = document.getElementById("showFormBtn")
 const form = document.getElementById("bookForm")
+const library = document.getElementById("renderLibrary")
 
 // Initialized the Form to be hidden on load.
 form.style.display = "none"
@@ -13,12 +14,8 @@ let newBook
 function Book(author, title, numPages, read = false) {
     this.author = author,
     this.title = title,
-    this.numPages = numPages + "pg.",
+    this.numPages = numPages,
     this.read = read
-}
-
-function addBookToLibrary() {
-    // do stuff here
 }
 
 // Eventlistener to display Book form when button is clicked.
@@ -36,18 +33,30 @@ form.addEventListener("submit", (event) => {
         formValue.author.value,
         formValue.title.value,
         formValue.numPages.value,
-        form.read.value
+        formValue.read.checked
     )
 
-
+    // Pushes Obj to library array.
     myLibrary.push(newBook)
-    alert(JSON.stringify(newBook))
+    // Reset the form fields after submission.
+    form.reset()
+    alert(JSON.stringify(newBook)) // FOR DEBUGGING DELETE LATER!!
     return false
 })
 
+const renderBook = () => {
+    let pNode = document.createElement("p")
 
 
+    myLibrary.map(book => {
+        pNode.appendChild(book.author)
+        pNode.appendChild(book.title)
+        library.appendChild(pNode)
+    })
+}
+
+// Eventlistener for when the Library changes.
 
 
-
-document.getElementById("debugLibrary").addEventListener("click", () => alert(JSON.stringify(myLibrary)))
+document.getElementById("debugLibrary").addEventListener("click", () => alert(JSON.stringify(myLibrary))) // FOR DEBUGGING DELETE LATER!!
+// document.getElementById("debugLibrary").addEventListener("click", renderBook) // FOR DEBUGGING DELETE LATER!!
