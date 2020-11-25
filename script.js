f47f52c7d0a7f021b86b1bdaec1da37b2c2dbc67
tien96ng <tien96ng@gmail.com>
2020-11-24 19:27:45 -0800

    Added a debug btn for the library and onsubmit working to add to Library.

--- script.js
+++ script.js
@@ -5,6 +5,22 @@ const form = document.getElementById("bookForm")
 // Initialized the Form to be hidden on load.
 form.style.display = "none"
 
+// Initialize the Library Array and new book variable.
+let myLibrary = []
+let newBook
+
+// Book Constructor:
+function Book(author, title, numPages, read = false) {
+    this.author = author,
+    this.title = title,
+    this.numPages = numPages + "pg.",
+    this.read = read
+}
+
+function addBookToLibrary() {
+    // do stuff here
+}
+
 // Eventlistener to display Book form when button is clicked.
 showFormBtn.addEventListener("click", () => {
     form.style.display === "none" ?
@@ -14,30 +30,24 @@ showFormBtn.addEventListener("click", () => {
 
 // Eventlistener to read inputs on form submission.
 form.addEventListener("submit", (event) => {
-    // event.target.elements.(ELEMENT ID).checked
     event.preventDefault()
-    
+    let formValue = event.target.elements
+    newBook =  new Book(
+        formValue.author.value,
+        formValue.title.value,
+        formValue.numPages.value,
+        form.read.value
+    )
+
+
+    myLibrary.push(newBook)
+    alert(JSON.stringify(newBook))
     return false
 })
 
 
-// Initialize the Library Array and new book variable.
-let myLibrary = []
-let newBook
-
-// Book Constructor:
-function Book(author, title, numPages, read = false) {
-    author = this.author,
-    title = this.title,
-    numPages = this.numPages + "pg.",
-    read = this.read
-}
-
-function addBookToLibrary() {
-    // do stuff here
-}
 
 
-const showForm = () => {
 
-}
+
+document.getElementById("debugLibrary").addEventListener("click", () => alert(JSON.stringify(myLibrary)))
